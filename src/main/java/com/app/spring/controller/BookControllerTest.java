0f6791c0ce195c5b1443cb42df1f530780f272e0
= <=>
2019-07-02 17:26:29 +0200

    Add libraries for JUnit

--- src/main/java/com/app/spring/controller/BookControllerTest.java
+++ src/main/java/com/app/spring/controller/BookControllerTest.java
@@ -1,27 +1,22 @@
 package com.app.spring.controller;
 
-import com.app.spring.repository.BookRepository;
+
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
 public class BookControllerTest {
 
-    @Mock
-    BookRepository bookRepository;
-
-    @InjectMocks
-    BookController bookController;
-
-
     @Test
-    public void get_all_books() {
-
-
+    public void should_add_two_numbers() {
+        assertEquals(2 + 2, 4);
     }
 
-    @Test
-    public void add_new_book() {
-    }
+
 }

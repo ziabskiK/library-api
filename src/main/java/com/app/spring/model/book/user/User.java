f406605762067db3dcb5006ba89f1452a37682b8
= <=>
2019-07-30 22:03:14 +0200

    Implemented User model.

--- src/main/java/com/app/spring/model/book/user/User.java
+++ src/main/java/com/app/spring/model/book/user/User.java
@@ -1,4 +1,54 @@
 package com.app.spring.model.book.user;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name = "user")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NonNull
+    @Column(name = "password")
+    private String password;
+
+    @NonNull
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NonNull
+    @Column(name = "first_name")
+    private String firstName;
+
+    @NonNull
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "ROLE")
+    private String role = "USER";
+
+
+    public User(@NonNull String password, @NonNull String email) {
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(@NonNull String password, @NonNull @Email String email, @NonNull String firstName, @NonNull String lastName) {
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.role = "USER";
+    }
 }

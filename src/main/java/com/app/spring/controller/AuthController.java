d7bd87329136a1a47031ec0fe2acd498bb7f2224
= <=>
2019-07-30 22:02:16 +0200

    Added login and register services

--- src/main/java/com/app/spring/controller/AuthController.java
+++ src/main/java/com/app/spring/controller/AuthController.java
@@ -2,19 +2,30 @@ package com.app.spring.controller;
 
 
 import com.app.spring.model.book.user.User;
+import com.app.spring.service.auth.LoginService;
+import com.app.spring.service.auth.RegisterService;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+    private LoginService loginService;
+    private RegisterService registerService;
+
+    public AuthController(LoginService loginService, RegisterService registerService) {
+        this.loginService = loginService;
+        this.registerService = registerService;
+    }
+
     @PostMapping("/login")
-    public String login() {
-        return "login";
+    public String login(@RequestBody User user) {
+        return loginService.login(user);
     }
 
     @PostMapping
-    public User register() {
-        return null;
+    public String register(@RequestBody User user) {
+        return registerService.register(user);
     }
 }

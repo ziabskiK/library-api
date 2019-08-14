c4acad97b4721da4f99216a9ca1b5033b5144771
= <=>
2019-08-14 13:08:39 +0200

    Removed query annotation.

--- src/main/java/com/app/spring/repository/BookRepository.java
+++ src/main/java/com/app/spring/repository/BookRepository.java
@@ -1,9 +1,7 @@
 package com.app.spring.repository;
 
 import com.app.spring.model.book.Book;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,11 +9,9 @@ import java.util.List;
 @Repository
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
-    @Query("SELECT b from Book b where b.authorLastName =:name")
-    List<Book> findBooksByAuthorLastName(@Param("name") String name);
+    List<Book> findBooksByAuthorLastName(String name);
 
-    @Query("SELECT b from Book b where b.title =:title")
-    List<Book> findBookByTitle(@Param("title") String title);
+    List<Book> findBookByTitle(String title);
 
 
 }
--- src/main/java/com/app/spring/repository/UserRepository.java
+++ src/main/java/com/app/spring/repository/UserRepository.java
@@ -1,12 +1,11 @@
 package com.app.spring.repository;
 
-import com.app.spring.model.book.user.User;
+import com.app.spring.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    @Query("SELECT u FROM User u where u.email=:email")
-    User findUserByEmail(@Param("email") String email);
+    User findUserByEmail(String email);
+
+
 }
